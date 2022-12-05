17dddeda3093a74c9d95763071e72d86a842b818
SudamAtCAW <sudam.swain@cawstudios.com>
2022-12-05 16:53:46 +0530

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 30000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true
+  }
+});
